d2c0e68e2ad1631435eafbd20d4874f3d5a66896
Jenny <jchudesign0930@gmail.com>
2020-09-17 19:03:56 -0400

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Plant.create(name: 'hello', amount:20)
+Plant.create(name: 'hello', amount:20, date: 'Sep 16, 2020')
